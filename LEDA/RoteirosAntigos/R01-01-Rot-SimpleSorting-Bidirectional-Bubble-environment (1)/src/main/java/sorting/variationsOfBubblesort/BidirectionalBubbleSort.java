59be494f9d25c13b6e5f0f132ccafc13c738e37b
igortejo <igortejo5@gmail.com>
2023-03-28 17:25:54 -0300

    mandando

--- LEDA/RoteirosAntigos/R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment (1)/src/main/java/sorting/variationsOfBubblesort/BidirectionalBubbleSort.java
+++ LEDA/RoteirosAntigos/R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment (1)/src/main/java/sorting/variationsOfBubblesort/BidirectionalBubbleSort.java
@@ -7,7 +7,7 @@ import util.Util;
  * This bubble sort variation has two internal iterations. In the first, it
  * pushes big elements to the right, like the normal bubble sort does. Then in
  * the second, iterates the array backwards, that is, from right to left, while
- * pushing small elements to the left. This process is repeated until the array
+ * pushing small elements to the left. This process is repeated until the arr	ay
  * is sorted.
  */
 public class BidirectionalBubbleSort<T extends Comparable<T>> extends
